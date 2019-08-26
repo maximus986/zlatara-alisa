448e61a7ccf9ab7bf6756bd427a41d512484d40a
Aleksandar Maksimovic <aleksandar.maksimovic@p3-holding.com>
2019-08-26 23:56:48 +0200

    Install sass plugin

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,8 +1,10 @@
 import React from 'react'
+import styles from '../styles/layout.module.scss'
 
 export default function Layout({children}) {
   return (
     <div>
+    <h1 className={styles.title}>Lorem ipsum dolor sit amet.</h1>
       {children}
     </div>
   )
