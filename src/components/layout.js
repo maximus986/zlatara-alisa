f68af976c6042129711090b270de9f0bcf4b35e1
Aleksandar Maksimovic <aleksandar.maksimovic@p3-holding.com>
2019-08-29 21:49:33 +0200

    Add base style

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,10 +1,9 @@
 import React from 'react'
-import styles from '../styles/layout.module.scss'
+import '../styles/layout.css';
 
 export default function Layout({children}) {
   return (
     <div>
-    <h1 className={styles.title}>Lorem ipsum dolor sit amet.</h1>
       {children}
     </div>
   )
