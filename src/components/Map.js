8ebf6b2149b4c2e4837ecec332c06bc6f3558069
Aleksandar Maksimovic <aleksandar.maksimovic@p3-holding.com>
2019-10-05 20:28:05 +0200

    Add google maps

---
+++ src/components/Map.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react'
+import {
+  withScriptjs,
+  withGoogleMap,
+  GoogleMap,
+  Marker, } from 'react-google-maps';
+  import MapStyles from '../static-data/mapStyles';
+  import styles from '../styles/contact.module.css';
+
+export default class Map extends Component {
+  render() {
+    const GoogleMapZA = withScriptjs(withGoogleMap(props => (
+      <GoogleMap
+        defaultCenter={{ lat: 44.808788, lng: 20.482993 }}
+        defaultZoom = { 17 }
+        options={{ styles: MapStyles }}
+        >
+          <Marker position={{ lat: 44.808788, lng: 20.482993 }}/>
+        </GoogleMap>
+    )))
+    return (
+      <GoogleMapZA
+        googleMapURL={`https://maps.googleapis.com/maps/api/js?key=${process.env.GOOGLE_API_KEY}&v=3.exp&libraries=geometry,drawing,places`}
+        loadingElement={<div style={{ height: `100%` }} />}
+        containerElement={ <div style={{ height: `500px`, width: '100%' }} className={styles.map}/> }
+        mapElement={ <div style={{ height: `100%` }} /> }
+      />
+    )
+  }
+}
--- src/pages/kontakt.js
+++ src/pages/kontakt.js
@@ -3,12 +3,25 @@ import { graphql } from 'gatsby';
 import Layout from '../components/layout';
 import Hero from '../components/Hero';
 import SEO from '../components/seo';
+import Map from '../components/Map';
+import Container from '../styled-components/Container';
+import styles from '../styles/contact.module.css';
 
 const kontakt = ({data}) => {
   return (
     <Layout>
       <SEO title="Kontakt" />
       <Hero img={data.contactBg.childImageSharp.fluid}/>
+      <Container className={styles.container}>
+        <section className={styles.contact}>
+          <article className={styles.contact__details}>
+            <h2 className={styles.contact__details__title}>Kontakt</h2>
+          </article>
+          <article className={styles.contact__map}>
+            <Map />
+          </article>
+        </section>
+      </Container>
     </Layout>
   );
 }
