e2f66cc7355436059d13698986ff2c015a2c842e
Aleksandar Maksimovic <aleksandar.maksimovic@p3-holding.com>
2019-10-12 16:47:23 +0200

    Implement about us page

--- src/pages/o-nama.js
+++ src/pages/o-nama.js
@@ -2,24 +2,78 @@ import React from 'react';
 import { graphql } from 'gatsby';
 import Layout from '../components/layout';
 import Hero from '../components/Hero';
+import Title from '../components/Title';
+import Container from '../styled-components/Container';
 import SEO from '../components/seo';
+import Img from 'gatsby-image';
+import styles from '../styles/about.module.css';
 
 const oNama = ({data}) => {
+  const images = data.about.edges;
   return (
     <Layout>
       <SEO title="O nama" />
       <Hero img={data.aboutBg.childImageSharp.fluid}/>
+      <section className={styles.about}>
+        <Container>
+          <Title title="ovo je naša priča" />
+          <figure className={styles.aboutImgWrapper}>
+            <Img fluid={images[0].node.childImageSharp.fluid} className={styles.aboutLeadImg} />
+          </figure>
+          <p className={styles.aboutText}>
+            Još davne 1972. godine Nuhi Husnijar osnovao je časovničarsko-zlatarsku radnju.
+            I dan danas ovaj porodični posao obavljamo revnosno, predano i profesionalno.
+            Delatnost radnje proširena je 2001. godine.
+          </p>
+          <figure className={styles.aboutImgWrapper}>
+            <Img fluid={images[2].node.childImageSharp.fluid} className={styles.aboutImg} />
+          </figure>
+          <figure className={styles.aboutImgWrapper}>
+            <Img fluid={images[3].node.childImageSharp.fluid} className={styles.aboutImg} />
+          </figure>
+          <p className={styles.aboutText}>
+            Bavimo se servisom, popravkom i prodajom svih vrsta časovnika: ručnih (analognih i kvarcnih),
+            zidnih, stonih i kaminskih. Vršimo zamenu kaiša i narukvica, poliranje i zamenu stakla na časovnicima.
+          </p>
+          <p className={styles.aboutText}>
+            Po veoma povoljnim cenama kod nas možete izvršiti zamenu starog zlata za novo, ali i doneti nam lomljeno zlato
+            jer se bavimo i otkupom. Izlazimo Vam u susret i ispunjavamo Vaše želje praveći sve vrste nakita po narudžbini.
+            Pored nakita kod nas možete naručiti i kovane zlatnike, sve po Vašoj ideji.
+          </p>
+          <figure className={styles.aboutImgWrapper}>
+            <Img fluid={images[1].node.childImageSharp.fluid} className={styles.aboutImg} />
+          </figure>
+          <p className={styles.aboutText}>
+            Kod nas možete pronaći veliki izbor prstenja, burmi, ogrlica, narukvica, privezaka, lančića, satova, minđuša...
+            Verujemo da ćete naći nešto što odražava Vašu ličnost.
+            Pored kupovine kod nas možete Vašem nakitu vratiti stari sjaj,jer nudimo i usluge poliranja,
+            ao i zamenu i fasovanje (postavljanje) kamenja na nakitu.
+          </p>
+
+        </Container>
+      </section>
     </Layout>
 );
 }
 
 export default oNama;
 export const query = graphql`
-{
-  aboutBg: file(relativePath: {eq: "bg-11.webp"}) {
+query {
+  aboutBg: file(relativePath: { eq: "bg-11.webp" }) {
     childImageSharp {
-      fluid(maxWidth: 4160, quality:90) {
+      fluid(maxWidth: 4160, quality: 90) {
         ...GatsbyImageSharpFluid
+      }
+    }
+  }
+  about: allFile(filter: { relativeDirectory: { eq: "aboutUs" } }) {
+    edges {
+      node {
+        childImageSharp {
+          fluid {
+            ...GatsbyImageSharpFluid
+          }
+        }
       }
     }
   }
