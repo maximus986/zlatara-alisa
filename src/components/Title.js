e4faf713056f5dfa4a1704010027c30c3f0fcfc1
Aleksandar Maksimovic <aleksandar.maksimovic@p3-holding.com>
2019-10-15 21:30:39 +0200

    Add title component styled border bottom

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import styled from 'styled-components';
+import diamond from '../images/favicon.png';
 
 const Title = ({title, className}) => {
   return (
     <div className={className}>
       <h2 className="title">
         {title}
+        <figure className="title-img-wrapper">
+          <img src={diamond} alt="Diamond" className="title-img"/>
+        </figure>
       </h2>
     </div>
   );
@@ -16,20 +20,49 @@ export default styled(Title)`
   font-size: 1.8rem;
   @media (min-width: 768px) {
     font-size: 2rem;
+    margin-bottom: 70px;
   }
   font-weight: 300;
   letter-spacing: 5px;
   text-transform: uppercase;
   padding-bottom: 15px;
-  margin-bottom: 40px;
+  margin-bottom: 50px;
   color: var(--main-dark);
-    &:after {
+  position: relative;
+  &-img-wrapper {
+    display: block;
+    position: absolute;
+    bottom: -30%;
+    left: 50%;
+    transform: translateX(-50%);
+    &:after,
+    &:before {
       content: "";
-      width: 90px;
+      width: 60px;
       height: 1px;
+      position: absolute;
+      top: 11px;
       display: block;
       background: var(--main-color);
-      margin: 15px auto 0;
     }
+    &:after {
+      left: 30px;
+    }
+    &:before {
+      right: 30px;
+    }
+    @media (min-width:576px) {
+      bottom: -30px;
+      &:after,
+      &:before {
+        width: 90px;
+        top: 13px;
+      }
+    }
+  }
+  &-img {
+    width: 25px;
+  }
+
   }
 `;
